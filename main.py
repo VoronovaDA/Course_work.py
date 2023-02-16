42a3eebaca8adfab7586cb168aa9dddb6c576685
Darya _V_13 <118071419+VoronovaDA@users.noreply.github.com>
2023-02-16 19:12:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from time import sleep
 
 config = configparser.ConfigParser()
 config.read("settings.ini")
-access_token = config.get('token', 'access_token')
-user_name = config.get('token', 'user_name')
-token_ya = config.get('token', 'token_ya')
-# print(access_token, user_name, token_ya)
+access_token = config["token"]["access_token"]
+user_name = config["token"]["user_name"]
+token_ya = config["token"]["token_ya"]
+print(access_token, user_name, token_ya)
 
 
 def time_convert(time_unix):
@@ -31,49 +31,32 @@ class VK:
             'owner_id': self.id,
             'v': self.version}
 
- 
-      
-    # def get_users_name(self, user_name):
-    #     url = 'https://api.vk.com/method/utils.resolveScreenName'
-    #     params = {
-    #         'access_token': access_token,
-    #         'screen_name': user_name,
-    #         'v': '5.131'
-    #     }
-    #     response = requests.get(url, params=params)
-    #     if 'user_name'.isdigit():
-    #         return user_name
-    #     else:
-    #         n_n = response.json()['response']['odject_id']
-    #         return n_n
-
-
+        
     def users_info(self, user_name):
         url = 'https://api.vk.com/method/users.get'
-        params = {'user_ids': self.id}
+        params = {'user_ids': user_name}
         response = requests.get(url, params={**self.params, **params}).json()
-        user_name =  response['response']['id']
-        return user_name
+        id=([i[k] for i in  response['response'] for k in i])
+        return id
+        
 
-
-    def json_file(self, user_name, album_id, f_count):
+    def json_file(self, user_name):
         url = 'https://api.vk.com/method/photos.get'
-        params = {'owner_id': user_name,
+        params = ({'owner_id': user_name,
                   'album_id': album_id,
                   'count': f_count,
                   'rev': 0,
                   'extended': 1,
                   'photo_sizes': 1
-                  }
-        response = requests.get(url, params={**self.params, **params})
-        data = response.json()['response']['items']
+                  })
+        res = requests.get(url=url, params={**self.params, **params}).json()
+        data = res['response']['items']
         return data
 
-
-    def photos_size(self):
+    def parsed_photo(self):
         max_size = {}
-        data = vk.json_file(user_name, album_id, f_count)
-        for ph in data:
+        data_f = vk.json_file(user_name)
+        for ph in data_f['response']['items']:
             file_name = f'{ph["likes"]["count"]}.jpg'
             time_warp = time_convert(ph['date'])
             file_names = f'{ph["likes"]["count"]}{time_warp}.jpg'
@@ -101,30 +84,36 @@ class Yandex:
 
     def ya_path_folder(self, folder_name):
         self.ya_url = "https://cloud-api.yandex.net/v1/disk/resources"
-        upload_photo(self)
         if requests.get(f'{self.ya_url}?path={folder_name}', headers=self.headers).status_code != 200:
             requests.put(f'{self.ya_url}?path={folder_name}',
                          headers=self.headers)
             print(f'\nПапка {folder_name} успешно создана!\n')
         else:
-            print(f'\nПапка {folder_name} уже существует\n')
+            print(f'\nПапка {folder_name} уже существует!\n')
         return folder_name
 
 
-def upload_photo(self):
-    file_json = []
-    _photo = vk.photos_size()
-    for k, dict_v in tqdm(_photo.items(), desc='Идет копирование фотографий: ', colour='#00ff00'):
-        sleep(0.1)       
-        self.path_file = f'/{folder_name}/{k}.jpg'
-        self.params = {'path': self.path_file, 'url': dict_v.values()}
-        for key in dict_v.keys():
-            json_photo = {"file_name": f'{k}.jpg', "size": f'{key}'}
-            file_json.append(json_photo) 
-    return  file_json
+    def upload_photo(self, folder_name):
+        upload_url = self.ya_url + 'upload'
+        file_json = []
+        _photo = vk.json_file(user_name)
+        for k, dict_v in tqdm(_photo.items(), desc='Идет копирование фотографий: ', colour='#00ff00'):
+            sleep(0.1) 
+            path_file = f'/{folder_name}/{k}.jpg'              
+            self.params = {'path': path_file, 'url': dict_v.values()}
+            res = requests.post(upload_url, params=self.params, headers=self.headers)
+            status = res.status_code
+            for key in dict_v.keys():
+                json_photo = {"file_name": f'/{folder_name}/{k}.jpg', "size": f'{key}'}
+                file_json.append(json_photo) 
+        with open('json_photo.json', 'a') as f:
+            json.dump(file_json, f, indent=0)
 
-
-# user_name = input('Введите id пользователя VK или Screen_Name ): ')
+        if 400 > status:
+            print(f'Фотографии загружены на Я-Диск!')
+        else:
+            print('Ошибка загрузки!')
+        
 
 vk = VK(access_token, user_name)
 ya = Yandex(token_ya)
@@ -146,18 +135,9 @@ while True:
         f_count = 5
         print('Установленно значение по умолчанию - 5 фото.')
     folder_name = input('Введите название папки для сохранения фото: ')
-    file_up = []
+    json_photo = vk.json_file(user_name)
+    parsed_photo = vk.parsed_photo(json_photo)
     # token_ya = input("""Введите токен от Я-Диска: """)
-    if requests.get(f'{ya.ya_url}?path={folder_name}', headers=ya.headers).status_code == 403:
+    if requests.get(f'{ya.ya_url}?path={folder_name}', headers=ya.headers).status_code > 400:
         print('Введены неверные данные, проверьте Ваш токен!')
         break
-    else:
-        j_file = upload_photo(folder_name)
-  
-    file_name = 'VK_photo.json'
-    with open(file_name, 'w') as f:
-        json.dump(file_up, f, indent=2)
-    with open(folder_name, 'w') as t:
-        t.write(file_name)
-    print('Загрузка фото прошла успешно!')
-    break
